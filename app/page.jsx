aedb5e96519b8f5e3317ee349456705b8b2bcd75
GigiBeridzeAxal <145029127+GigiBeridzeAxal@users.noreply.github.com>
2024-04-18 13:06:09 +0400

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+
+const EmployeeLocation = () => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+          },
+          (error) => {
+            setError(error.message);
+          }
+        );
+      } else {
+        setError('Geolocation is not supported by this browser.');
+      }
+    };
+
+    getLocation();
+
+    // Clean up the geolocation watcher when the component unmounts
+    return () => {
+      navigator.geolocation;
+    };
+  }, []);
+
+  return (
+    <div>
+      {error ? (
+        <p>Error: {error}</p>
+      ) : (
+        <>
+          <p>Latitude: {latitude}</p>
+          <p>Longitude: {longitude}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default EmployeeLocation;
